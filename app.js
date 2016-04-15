ae22e1b3d06cdbd58e85d945550e97d0115aab41
Margaret Morgan <mmogan@datto.com>
2016-04-15 14:44:10 -0400

    Initial setup commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!');
+});
+
+// Reminder for basic express setup
+// mkdir myapp
+// cd myapp
+// npm init
+
+// npm install express --save
+
+// copy app.js
+// copy index.html
+// copy public folder structure
+
+// run 'node app.js' to start app
