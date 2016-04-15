a2205fb75b78acce95bcca12aadefff424a74a00
Margaret Morgan <mmogan@datto.com>
2016-04-15 15:45:01 -0400

    Added basic routing pattern for project folders

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/projects/:projectname', function (req, res) {
+  var projectName = req.params.projectname;
+  res.sendFile(path.join(__dirname + '/public/projects/' + projectName + '/index.html'));
+});
+
 app.listen(4000, function () {
   console.log('Example app listening on port 4000!');
 });
